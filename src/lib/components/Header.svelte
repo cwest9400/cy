1a812e4d9c422360022b5887ed917369dd3f5719
cwest9400 <chrstphrwest@gmail.com>
2025-03-18 11:58:39 -0400

    navbar update

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<header>
+    <nav class="nav-container"></nav>
+      <div class="logo-container"></div>
+      <ul class="link-container">
+        <li><a href="/services&products">SERVICES & PRODUCTS</a></li>
+        <li><a href="/industries">INDUSTRIES</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+         font: white;
+         background-color: black;
+
+    }
+
+    .nav-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .logo-container {
+max-width: 350 px;
+    }
+
+    .link-container {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
